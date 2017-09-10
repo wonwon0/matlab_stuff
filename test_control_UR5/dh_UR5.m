aeb3feeabac70891abfe54b9b1c60346436a4bbf
philippe lebel <philippe.lebel008@sympatico.ca>
2017-09-10 18:36:33 -0400

    ajout de param dh variable pour accomoder une effecteur pour le robot ur_5

--- test_control_UR5/dh_UR5.m
+++ test_control_UR5/dh_UR5.m
@@ -1,4 +1,4 @@
-function [ dh ] = dh_UR5(link, ratio)
+function [ dh ] = dh_UR5(link, ratio, end_effector_offset)
 % link and ratio parameters allow to output variable dh parameters
 % according to a point of interest located on one of the robot's links.
 
@@ -6,9 +6,12 @@ function [ dh ] = dh_UR5(link, ratio)
 % from 1 up to 6), and the ratio tells where the point is located on the
 % link (from the base joint to the end joint of the link).
 
-if nargin < 2
-  link = 6;
-  ratio = 1;
+if nargin < 3
+    if nargin < 2 
+      link = 6;
+      ratio = 1;
+    end
+    end_effector_offset = 0;
 end
 
 modifier = zeros(1,6)';
@@ -17,7 +20,7 @@ modifier(link) = ratio;
 
 %output les param dh du robot UR5
 dh.a=[0 -425 -392.25 0 0 0]' .* modifier;
-dh.b=[89.159 0 0 109.15 94.65 82.3]' .* modifier;
+dh.b=[89.159 0 0 109.15 94.65 82.3+end_effector_offset]' .* modifier;
 dh.alpha=[pi/2 0 0 pi/2 -pi/2 0]';
 dh.theta=[0 -pi/2 0 -pi/2 0 0]';
 
